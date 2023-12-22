68f42e2a6566ff88268a077c091da9b883696c49
Renato Rios <renato_rq_987@hotmail.com>
2023-12-22 15:03:27 -0500

    se agrego logger

--- src/main/java/com/nttdata/supplier/controller/ProveedorController.java
+++ src/main/java/com/nttdata/supplier/controller/ProveedorController.java
@@ -3,6 +3,8 @@ package com.nttdata.supplier.controller;
 
 import com.nttdata.supplier.model.Proveedor;
 import com.nttdata.supplier.service.ProveedorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,8 @@ import java.util.List;
 @RequestMapping("/proveedores")
 public class ProveedorController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProveedorController.class);
+
     @Autowired
     private ProveedorService proveedorService;
 
@@ -20,6 +24,7 @@ public class ProveedorController {
     public ResponseEntity<Object> registrar(@RequestBody Proveedor p) {
 
         try {
+            LOGGER.info(String.format("Registro de Proveedor -> %s", p.toString()));
             proveedorService.registroProveedor(p);
             return ResponseEntity.ok(p);
         } catch (Exception e) {
@@ -30,6 +35,7 @@ public class ProveedorController {
     @PutMapping("/actualizar")
     public ResponseEntity<Object> actualizar(@RequestBody Proveedor p) throws Exception {
         try {
+            LOGGER.info(String.format("Actualizacion de Proveedor -> %s", p.toString()));
             Proveedor proveedor = proveedorService.actualizarProveedor(p);
             return ResponseEntity.ok(proveedor);
         } catch (Exception e) {
@@ -39,11 +45,13 @@ public class ProveedorController {
 
     @GetMapping("/buscar")
     public ResponseEntity<Proveedor> buscar(@RequestParam("id") int id) throws Exception {
+        LOGGER.info(String.format("Buscar de Proveedor"));
         return ResponseEntity.ok(proveedorService.buscarProveedor(id));
     }
 
     @GetMapping("/listar")
     public ResponseEntity<List<Proveedor>> listar() throws Exception {
+        LOGGER.info(String.format("Listar Proveedor"));
         return ResponseEntity.ok(proveedorService.listarProveedores());
     }
 
